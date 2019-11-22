9455eb994611271badb2bf3a960a01db8fc25608
khachapuridze <giorgi.khatchapuridze.1@btu.edu.ge>
2019-11-22 23:32:04 +0400

    fix components :milky_way:

--- project/src/components/LoadingBlock.vue
+++ project/src/components/LoadingBlock.vue
@@ -1,62 +1,62 @@
 <template lang="html">
-    <div id="loading-block"
-        :class="{
+  <div id="loading-block"
+       :class="{
             'one': one,
             'two': two,
             'three': three,
             'failed': failed
             }"
-    ></div>
+  ></div>
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            one: false,
-            two: false,
-            three: false,
-            timer: null
-        }
-    },
-    props: {
-        done: {
-            type: Boolean,
-            default: false
-        },
-        failed: {
-            type: Boolean,
-            default: false
-        }
-    },
-    watch: {
-        done(val) {
-            if (val) {
-                this.three = true;
+    export default {
+        data() {
+            return {
+                one: false,
+                two: false,
+                three: false,
+                timer: null
             }
         },
-        failed(val) {
-            if (val) {
-                this.failed = true;
+        props: {
+            done: {
+                type: Boolean,
+                default: false
+            },
+            failed: {
+                type: Boolean,
+                default: false
             }
+        },
+        watch: {
+            done(val) {
+                if (val) {
+                    this.three = true;
+                }
+            },
+            failed(val) {
+                if (val) {
+                    this.failed = true;
+                }
+            }
+        },
+        mounted() {
+            this.$nextTick(() => {
+                this.timer = setTimeout(() => this.one = true, 17);
+                this.timer = setTimeout(() => this.two = true, 500);
+            });
+        },
+        destroyed() {
+            this.$nextTick(() => {
+                this.timer && clearTimeout(this.timer);
+            });
         }
-    },
-    mounted() {
-        this.$nextTick(() => {
-            this.timer = setTimeout(() => this.one = true, 17);
-            this.timer = setTimeout(() => this.two = true, 500);
-        });
-    },
-    destroyed() {
-        this.$nextTick(() => {
-            this.timer && clearTimeout(this.timer);
-        });
     }
-}
 </script>
 
 <style lang="less">
-#loading-block {
+  #loading-block {
     height: 3px;
     width: 100%;
     background-color: #46a42f;
@@ -66,25 +66,25 @@ export default {
     transform-origin: 0 0;
 
     &.failed {
-        background-color: #f44336;
-        transform: scaleX(1) !important;
-        transition: transform 0.3s !important;
+      background-color: #f44336;
+      transform: scaleX(1) !important;
+      transition: transform 0.3s !important;
     }
 
     &.one {
-        transform: scaleX(0.2);
-        transition: transform 0.5s;
+      transform: scaleX(0.2);
+      transition: transform 0.5s;
     }
 
     &.two {
-        transform: scaleX(0.8);
-        transition: transform 20s cubic-bezier(0, 1.17, 1, 1);
+      transform: scaleX(0.8);
+      transition: transform 20s cubic-bezier(0, 1.17, 1, 1);
     }
 
     &.three {
-        transform: scaleX(1);
-        transition: transform 0.3s;
+      transform: scaleX(1);
+      transition: transform 0.3s;
     }
-}
+  }
 
 </style>
--- project/src/components/MenuOpenstatehandler.vue
+++ project/src/components/MenuOpenstatehandler.vue
@@ -1,33 +1,34 @@
 <template lang="html">
-    <input type="checkbox" id="nav-menu-open-checkbox" :checked="open" />
+  <input type="checkbox" id="nav-menu-open-checkbox" :checked="open"/>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
-export default {
-    computed: {
-        ...mapGetters({
-            open: 'getOpenState'
-        })
-    }
-};
+    import {mapGetters} from 'vuex';
+
+    export default {
+        computed: {
+            ...mapGetters({
+                open: 'getOpenState'
+            })
+        }
+    };
 </script>
 
 <style lang="less">
-input#nav-menu-open-checkbox {
+  input#nav-menu-open-checkbox {
     display: none;
-}
+  }
 
-input#nav-menu-open-checkbox:checked ~ #menu-overlay {
+  input#nav-menu-open-checkbox:checked ~ #menu-overlay {
     transform: translate3d(0, 0, 0);
-}
+  }
 
-input#nav-menu-open-checkbox:checked ~ #main-content {
-  transform: translate3d(40%, 0, 0);
-}
+  input#nav-menu-open-checkbox:checked ~ #main-content {
+    transform: translate3d(40%, 0, 0);
+  }
 
-input#nav-menu-open-checkbox:checked ~ #nav-menu {
+  input#nav-menu-open-checkbox:checked ~ #nav-menu {
     opacity: 0.99;
     transform: translate3d(0, 0, 0);
-}
+  }
 </style>
